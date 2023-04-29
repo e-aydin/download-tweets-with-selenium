8440f66c9d8cb5f008453df7128a2d709b8af290
Enver Aydın <43873663+e-aydin@users.noreply.github.com>
2023-04-29 12:51:49 +0300

    Update logininfo.py
    regulation

--- logininfo.py
+++ logininfo.py
@@ -2,6 +2,10 @@ username="@username"
 
 password="password"
 
-keyword="Recep Tayyip Erdoğan"
-advanced_search=""
-#
+keyword="searched word"
+advanced_search=" "
+## min_replies:1
+# min_faves:1
+# min_retweets:1
+# until:2023-01-01
+# since:2022-02-01
